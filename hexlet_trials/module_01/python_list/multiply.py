62d0299d7badb26f572a2acab762bbe79b19b259
Viktor Sklyuev <sklyevv@yandex.ru>
2023-05-31 16:19:54 +0300

    Base

---
+++ hexlet_trials/module_01/python_list/multiply.py
@@ -0,0 +1,14 @@
+# Реализуйте функцию multiply(), которая принимает две матрицы
+# и возвращает новую матрицу - результат их произведения
+
+
+def matrix_rotate(matrix):
+    pass
+
+
+def multiply(matrix_one, matrix_two):
+    matrix_two = matrix_rotate(matrix_two)
+
+    for string in matrix_one:
+        for item in string:
+            pass
